a280eb6bf90dd83e0ba8440e72bf1068313f4da0
Eric Newton <eric.newton@lightmaker.com>
2016-09-30 14:06:03 -0400

    init command work

--- index-init.js
+++ index-init.js
@@ -1,9 +1,23 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var prompt = require('prompt');
 
 program
   .parse(process.argv)
   
 console.log('Initialize a cp-config.json');
 
+prompt.start();
+
+prompt.get(['apikey','username','password'], function(err,result) {
+  if(err) { return onErr(err); }
+  
+  console.log("prompt result",result);
+
+});
+
+function onErr(err) {
+  console.log(err);
+  return 1;
+}
