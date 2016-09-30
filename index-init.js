e0838448fa2ed8ad7e5f2d410d005c7ea27cb13a
Eric Newton <eric.newton@lightmaker.com>
2016-09-30 14:15:35 -0400

    refined the init command prompts

--- index-init.js
+++ index-init.js
@@ -7,13 +7,39 @@ program
   .parse(process.argv)
   
 console.log('Initialize a cp-config.json');
+console.log('Note that the CrownPeak AccessAPI requires the use of an API Key.  Contact CrownPeak support at support@crownpeak.com to request a key.');
+
+//https://github.com/flatiron/prompt#valid-property-settings
+var properties = [
+  { 
+    name: 'cms-instance-name',
+    message: 'Name of the CrownPeak instance'
+  }
+  ,{
+    name: 'apikey',
+    message: 'The API key to access the instance with'
+  }
+  ,{
+    name: 'username'
+    ,message: '(optional) Username to use to access'
+    
+  }
+  ,{
+    name: 'password'
+    ,hidden: true
+    ,replace: '*'
+    ,message: '(optional) Password to use to access'
+  }
+];
 
 prompt.start();
 
-prompt.get(['apikey','username','password'], function(err,result) {
+prompt.get(properties, function(err,result) {
   if(err) { return onErr(err); }
   
-  console.log("prompt result",result);
+  result["cms-instance-url"] = "https://cms.crownpeak.net/" + result["cms-instance-name"];
+  
+  console.log('result', result);
 
 });
 
