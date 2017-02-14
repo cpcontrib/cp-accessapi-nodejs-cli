1ae58bbbfc53d86a5a837118aa705652da1051ef
Eric Newton <eric.newton@lightmaker.com>
2017-02-14 12:42:22 -0500

    switched logging to use log4js

--- index-update.js
+++ index-update.js
@@ -3,12 +3,55 @@
 var program = require('commander');
 var prompt = require('prompt');
 var fs = require('fs');
+var util = require('util');
 var chalk = require('chalk');
 var Q = require('q');
+var log4js = require('log4js');
+
+var log = log4js.getLogger();
+
+if (fs.existsSync('./log4js.json')) {
+  log4js.configure('./log4js.json');
+} else if(process.env["LOG4JS_CONFIG"] !== undefined) {
+
+} else {
+  log4js.configure({
+    appenders: [
+      { 
+        type: 'console' , 
+        level: 'INFO', 
+        layout: { type:"pattern", pattern: " %[%m%]" } 
+      },
+    ]
+  })
+  log.setLevel('INFO');
+}
 
 var constants = {
   configJsonName: "accessapi-config.json"
 };
+
+function status(text) {
+  var argsArray = Array.prototype.slice.call(arguments);
+  log.info.apply(log, argsArray);
+  if (log.isInfoEnabled==false) {
+    var str = util.format.apply(null,argsArray);
+    process.stdout.write(str);
+    process.stdout.write('\n');
+  }
+}
+function fail(text) {
+  var argsArray = Array.prototype.slice.call(arguments);
+  log.fatal.apply(log, argsArray);
+  if (log.isFatalEnabled==false) {
+    var str = util.format.apply(null,argsArray);
+    process.stderr.write(str);
+    process.stderr.write('\n');
+  }
+}
+
+process.stdout.write("\n");
+process.stdout.write("Crownpeak CLI starting.\n");
 
 program
   .name('update')
@@ -30,23 +73,25 @@ program
 program
   .parse(process.argv)
 
-console.log('program.config=%s', program.config);
-console.log('program.assetPath=%s', program.assetPath);
+log.debug('program.config',program.config);
+log.debug('program.assetPath',program.assetPath);
 
 function getContent (program, encoding) {
-  console.log('getUpdateGram'.green);
+  if (log.isDebugEnabled) log.debug('begin reading content');
   var deferred = Q.defer();
 
   if (program.stdin) {
+    log.debug('reading content from stdin');
+  
     var stdin = process.stdin;
     var stdout = process.stdout;
     
     var inputChunks = [];
     
     stdin.on('data', function (data) {
+      if (log.isDebugEnabled) log.debug('chunk:',data);
       if (Buffer.isBuffer(data)) data = data.toString('utf8');
       inputChunks.push(data);
-      console.log('chunk', data)
     });
     
     stdin.on('end', function () {
@@ -54,7 +99,6 @@ function getContent (program, encoding) {
       
       try {
         var parsedData = JSON.parse(contentStr);
-        console.log('resolve', parsedData);
         deferred.resolve(parsedData);
         return;
       }
@@ -73,7 +117,7 @@ function getContent (program, encoding) {
   else //read from file
   {
     //read file name from program.args[2]
-    console.log('reading from file=%s', program.inputFile);
+    log.debug('reading from file=%s', program.inputFile);
     return Q.nfcall(fs.readFile, program.inputFile, { 'encoding': encoding });
   }
   
@@ -82,26 +126,35 @@ function getContent (program, encoding) {
 
 main = function () {
   if (typeof program.assetPath === 'undefined') {
+    fail('no assetPath specified.');
     program.help();
     process.exit(1);
   }
   
-  
-  console.log('read %s', program.config);
-  
+  log.debug('Loading config from %s.', program.config);
+  if (fs.existsSync(program.config)==false) {
+    fail('Failed to load config from %s: file doesn\'t exist.', program.config);
+    process.exit(1); 
+  }
+
   //var reader = require('./accessapi-json-config-reader');
   var accessapiConfig = JSON.parse(fs.readFileSync(program.config));
   
-  console.log('read config', accessapiConfig);
+  log.debug('accessapiConfig:', accessapiConfig);
+
+  status(`Instance: ${accessapiConfig.instance}   Sign in as: ${accessapiConfig.username}`);
+  status(`Updating: ${program.assetPath}`);
   
   var accessapi = require('crownpeak-accessapi');
   accessapi.setConfig(accessapiConfig);
   
-  console.log('calling auth');
+  status('');
+  status('Authenticating.');
   accessapi.auth().then(function (data) {
     
     var assetIdOrPath = program.assetPath;
 
+    log.debug('calling AssetExists');
     accessapi.AssetExists(assetIdOrPath).then(function (existsResp) {
       
       //existsResp documented http://developer.crownpeak.com/Documentation/AccessAPI/AssetController/Methods/Exists(AssetExistsRequest).html
@@ -109,14 +162,19 @@ main = function () {
       
       getContent(program).then(function (fieldsJson) {
         
-        accessapi.AssetUpdate(workflowAssetId, fieldsJson, null, /*runPostInput*/false, /*runPostSave*/true);
+        log.debug('calling AssetUpdate');
+        accessapi.AssetUpdate(workflowAssetId, fieldsJson, null, /*runPostInput*/false, /*runPostSave*/true).then(function() {
+          status('Success updating %s.', program.assetPath);
+        })
 
       });
 
     });
 
+  }, function(err) {
+    fail('Authentication failure: %s', err.resultCode);
   }).catch(function (err) {
-    console.log("error occurred:", err);
-  })
+    error("error occurred:", err);
+  }).done();
 
 }();
